9886fa82fbbf522604deac2df98addba37721ff8
sameh-k <pikachu10191@hotmail.com>
2024-04-10 04:45:16 +0200

    initial layout of pages

--- src/app/(main)/(pages)/layout.tsx
+++ src/app/(main)/(pages)/layout.tsx
@@ -4,8 +4,8 @@ type Props = { children: React.ReactNode };
 
 const Layout = (props: Props) => {
   return (
-    <div className="flex overflow-hidden h-screen">
-      <div className="w-full">{props.children}</div>
+    <div className="border-l-[1px] border-t-[1px] pb-20 h-screen rounded-l-3xl border-muted-foreground/20 overflow-scroll">
+      {props.children}
     </div>
   );
 };
